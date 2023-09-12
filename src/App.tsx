872fb8435e0513400d729a5ffa0366d1b742d8f0
Guillermina Lorenzo Martinez <glm1a23@soton.ac.uk>
2023-09-12 11:28:43 +0100

    Set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CounterDisplay from "./components/CounterDisplay";
+import CounterButtons from "./components/CounterButtons";
+import { CounterProvider } from "./context/CounterContext";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App</h1>
+      <CounterProvider>
+        <CounterDisplay />
+        <CounterButtons />
+      </CounterProvider>
     </div>
   );
 }
